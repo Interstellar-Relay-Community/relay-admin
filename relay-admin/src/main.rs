16e889a0b6b37eaf62dace157847ffe376a7da2d
perillamint <perillamint@silicon.moe>
2023-07-07 22:05:05 +0900

    Initial structure

--- relay-admin/src/main.rs
+++ relay-admin/src/main.rs
@@ -17,7 +17,40 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use std::net::SocketAddr;
+
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    response::IntoResponse,
+    Json, Router,
+};
+use diesel_async::{pooled_connection::AsyncDieselConnectionManager, AsyncPgConnection};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+mod api;
+
+type Pool = bb8::Pool<AsyncDieselConnectionManager<AsyncPgConnection>>;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
+
+    let db_url = std::env::var("DATABASE_URL").unwrap();
+
+    let config = AsyncDieselConnectionManager::<diesel_async::AsyncPgConnection>::new(db_url);
+    let pool = bb8::Pool::builder().build(config).await.unwrap();
+
+    let app = Router::new()
+        //.nest("/v1", api::v1::routes())
+        .with_state(pool);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
